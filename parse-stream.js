e9bfad513f57a4785659ff95260cb88d009e9f5c
Qiao Liang <qiaoliang89@gmail.com>
2016-05-11 23:23:31 +0800

    completed stream

--- parse-stream.js
+++ parse-stream.js
@@ -1,3 +1,47 @@
-// 0. Please use readline (https://nodejs.org/api/readline.html) to deal with per line file reading
-// 1. Then use the parse API of csv-parse (http://csv.adaltas.com/parse/ find the Node.js Stream API section)
+'use strict';
 
+const debug = require('debug')('hello');
+const parse = require('csv-parse');
+const helper = require('./helper');
+const readline = require('readline');
+const fs = require('fs');
+const parser = parse();
+
+parser.on('error', function (err) {
+    if (err) {
+        debug(err.message);
+    }
+});
+
+const rl = readline.createInterface({
+    input: fs.createReadStream(__dirname + '/sample.csv'),
+});
+
+var first = true;
+rl.on('line', function (line) {
+    if (first) {
+        first = false;
+        return;
+    }
+
+    let transformedLine = helper.transformLineToUseFullName(line.split(','));
+    debug(`sending data : ${transformedLine}`);
+
+    helper.sendSms(transformedLine, function afterSending(err, sendingStatus) {
+        if (err) {
+            debug(err.message);
+        }
+
+        let lineToLog = {
+            sendingStatus,
+            transformedLine,
+        };
+        helper.logToS3(lineToLog, function afterLogging(err, loggingStatus) {
+            if (err) {
+                debug(err.message);
+            }
+        });
+    });
+}).on('close', function () {
+    parser.end();
+});
